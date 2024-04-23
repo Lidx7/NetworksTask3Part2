fdb1de42f2de1a861528e077e380b7db83aeb1d3
Lidx7 <lidor.borofker@windowslive.com>
2024-04-23 21:23:50 +0300

    modified:   RUDP_API.c 	modified:   RUDP_Snder.c

---
+++ RUDP_API.c
@@ -0,0 +1,146 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/time.h>
+
+#define MAX_DATA_SIZE 1024
+#define MAX_RETRIES 5
+#define TRUE 1
+#define FALSE 0
+
+typedef struct {
+    int seq_num;
+    char data[MAX_DATA_SIZE];
+} Packet;
+
+typedef struct timeval {
+   long tv_sec;    /* seconds */
+   long tv_usec;   /* microseconds */
+};
+
+/*TODO: disolve these MACROs into function-specific micros*/
+struct sockaddr_in server_addr, client_addr;
+socklen_t addr_len = sizeof(struct sockaddr);
+char buffer[MAX_DATA_SIZE + sizeof(int)]; //TODO: change the sizeof section to represent the header (should use a MACRO)
+int seq_num = 0;
+/*could possibly add a packet loss send retries counter here*/
+
+int rudp_socket(int server_port) {
+    int sockfd;
+
+    // Create UDP socket
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Configure server address
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons (server_port);
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+
+    // Bind socket to server address
+    if (bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
+        perror("Socket bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Server waiting for client...\n");
+
+    // Handshake with client
+    recvfrom(sockfd, buffer, sizeof(buffer), 0, (struct sockaddr *)&client_addr, &addr_len);
+    printf("Handshake request received from client: %s\n", buffer);
+
+    // Send handshake acknowledgment
+    sendto(sockfd, "ACK", strlen("ACK"), 0, (struct sockaddr *)&client_addr, addr_len);
+
+    printf("Handshake complete. Waiting for data...\n");
+}
+
+int rudp_recv(){
+    int sockfd;
+
+    while (TRUE) {
+        // Receive packet from client
+        int bytes_received = recvfrom(sockfd, buffer, sizeof(buffer), 0, (struct sockaddr *)&client_addr, &addr_len);
+        if (bytes_received == -1) {
+            perror("Receive error");
+            continue;
+        }
+
+        Packet *packet = (Packet *)buffer;
+
+        // Check sequence number
+        if (packet->seq_num == seq_num) {
+            printf("Received packet with sequence number %d\n", seq_num);
+
+            // Acknowledge received packet
+            sendto(sockfd, &seq_num, sizeof(int), 0, (struct sockaddr *)&client_addr, addr_len);
+
+            // Print received data
+            printf("Data: %s\n", packet->data);
+
+            // Update sequence number
+            seq_num = (seq_num + 1) % 2;
+        } else {
+            printf("Received out-of-order packet. Discarding...\n");
+        }
+
+
+        /*
+        TODO: fix this packet loss simulation and replace it with the proper check
+        
+        // Simulate packet loss and retransmission
+        if (rand() % 10 < 3) { // 30% chance of packet loss
+            printf("Packet lost. Retrying...\n");
+            sendto(sockfd, &seq_num, sizeof(int), 0, (struct sockaddr *)&client_addr, addr_len);
+
+            if (retries == MAX_RETRIES) {
+                printf("Max retries reached. Aborting.\n");
+                break;
+            }
+        }
+        */
+    }
+}
+
+void rudp_send(const char *data, int sockfd;) {
+    Packet packet;
+    packet.seq_num = seq_num;
+    strncpy(packet.data, data, MAX_DATA_SIZE);
+
+    // Send packet
+    sendto(sockfd, &packet, sizeof(packet), 0, (struct sockaddr *)&client_addr, addr_len);
+    
+    // Wait for acknowledgment
+    while (TRUE) {
+        // Set timeout for acknowledgment
+        struct timeval tv;
+        tv.tv_sec = 1; // 1 second timeout
+        tv.tv_usec = 0;
+        setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, (const char*)&tv, sizeof(tv));
+
+        int ack_seq_num;
+        int bytes_received = recvfrom(sockfd, &ack_seq_num, sizeof(int), 0, (struct sockaddr *)&client_addr, &addr_len);
+        if (bytes_received != -1 && ack_seq_num == seq_num) {
+            printf("Packet with sequence number %d acknowledged.\n", seq_num);
+            break;
+        } else {
+            printf("Timeout. Retransmitting packet...\n");
+            sendto(sockfd, &packet, sizeof(packet), 0, (struct sockaddr *)&client_addr, addr_len);
+            
+        }
+    }
+}
+
+void rudp_close(int socket){
+    close(socket);
+    return;
+}
+
